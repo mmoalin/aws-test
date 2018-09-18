6ccab2263a5b77c27f7e945823b86b009d41ac7d
Mo Moalin <mmoalin@outlook.com>
2018-09-18 09:11:22 +0100

    express

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var APIProxy = require('../companiesHousesAPIProxy');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+/* GET home page. */
+router.get('/api/proxyCompanyHouse', function(req, res, next) {
+  let api = new APIProxy(res, (json) => {
+    this.res.send(json);
+  });
+  let url = "https://api.companieshouse.gov.uk/search/companies?q=AKZO+Nobel";
+  api.fetchUrl(url);
+ });
 module.exports = router;
